9f7dd526af48029934fc87b13d269d31ca04419f
zhangdaqing <18255182320@139.com>
2018-12-30 11:16:31 +0800

    还原初始状态

--- pinyougou-common/src/main/java/util/FastDFSClient.java
+++ pinyougou-common/src/main/java/util/FastDFSClient.java
@@ -6,10 +6,6 @@ import org.csource.fastdfs.StorageClient1;
 import org.csource.fastdfs.StorageServer;
 import org.csource.fastdfs.TrackerClient;
 import org.csource.fastdfs.TrackerServer;
-
-/**
- * 公共
- */
 public class FastDFSClient {
 
 	private TrackerClient trackerClient = null;
